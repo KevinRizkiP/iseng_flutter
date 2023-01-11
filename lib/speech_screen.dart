97c31a962bfe2b931bfae724068fa3748ef83f55
Kevin Rizki <kevinrizki002@gmail.com>
2023-01-11 11:58:24 +0700

    iseng voice to text

--- lib/speech_screen.dart
+++ lib/speech_screen.dart
@@ -1,5 +1,7 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:iseng/api_services.dart';
+import 'package:iseng/chat_model.dart';
 import 'package:iseng/colors.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 
@@ -14,6 +16,14 @@ class _SpeechScreenState extends State<SpeechScreen> {
   SpeechToText speechToText = SpeechToText();
   var text = "Hold the button and start speaking";
   var isListening = false;
+
+  final List<ChatMessage> messages = [];
+
+  var scrollController = ScrollController();
+  scrollMethod() {
+    scrollController.animateTo(scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,11 +54,17 @@ class _SpeechScreenState extends State<SpeechScreen> {
               }
             }
           },
-          onTapUp: (details) {
+          onTapUp: (details) async {
             setState(() {
               isListening = false;
             });
             speechToText.stop();
+            messages.add(ChatMessage(text: text, type: ChatMessageType.user));
+            var msg = await ApiServices.sendMessage(text);
+
+            setState(() {
+              messages.add(ChatMessage(text: msg, type: ChatMessageType.bot));
+            });
           },
           child: CircleAvatar(
             backgroundColor: bgColor,
@@ -69,22 +85,91 @@ class _SpeechScreenState extends State<SpeechScreen> {
           style: TextStyle(fontWeight: FontWeight.w600, color: textColor),
         ),
       ),
-      body: SingleChildScrollView(
-        reverse: true,
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          margin: const EdgeInsets.only(bottom: 150),
-          child: Text(
-            text,
-            style: TextStyle(
-                fontSize: 24,
-                color: isListening ? Colors.black87 : Colors.black54,
-                fontWeight: FontWeight.w600),
-          ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+        child: Column(
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                  fontSize: 24,
+                  color: isListening ? Colors.black87 : Colors.black54,
+                  fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Expanded(
+                child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12)),
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: scrollController,
+                itemCount: messages.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext context, int index) {
+                  var chat = messages[index];
+                  return chatBubble(chattext: chat.text, type: chat.type);
+                },
+              ),
+            )),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              text,
+              style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w400),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget chatBubble({required chattext, required ChatMessageType? type}) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      CircleAvatar(
+        backgroundColor: bgColor,
+        child: type == ChatMessageType.bot
+            ? Image.network(
+                'https://seeklogo.com/images/C/chatgpt-logo-02AFA704B5-seeklogo.com.png')
+            : const Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+      ),
+      const SizedBox(
+        width: 12,
+      ),
+      Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.only(bottom: 8),
+          decoration: BoxDecoration(
+              color: type == ChatMessageType.bot ? bgColor : Colors.white,
+              borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(12),
+                  bottomRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12))),
+          child: Text(
+            "$chattext",
+            style: TextStyle(
+                color:
+                    type == ChatMessageType.bot ? textColor : Colors.grey[800],
+                fontSize: 15,
+                fontWeight: FontWeight.w400),
+          ),
+        ),
+      )
+    ],
+  );
 }
